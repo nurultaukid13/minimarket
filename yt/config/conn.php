7ef0c39bca17bbf442712bbbc0561cefcb326be8
nurultaukid13 <nurultaukid17@gmail.com>
2023-05-31 11:53:55 +0700

    awal

--- yt/config/conn.php
+++ yt/config/conn.php
@@ -2,7 +2,7 @@
     $hostname   = "localhost";
     $username   = "root";
     $password   = "";
-    $database   = "inventaris";
+    $database   = "stockpro";
 
     $con = mysqli_connect($hostname, $username, $password, $database) or die (mysqli_error($con));
 ?>
