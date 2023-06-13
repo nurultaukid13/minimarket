f39aff69af8ab5077068846d3bff3a21687541e8
arinitr21 <135719130+arinitr21@users.noreply.github.com>
2023-06-13 19:29:13 +0700

    Update kategori.php

--- StockPro/process/kategori.php
+++ StockPro/process/kategori.php
@@ -10,9 +10,9 @@ if(isset($_POST['tambah'])){
     
     $insert = mysqli_query($con,"INSERT INTO kategori (nama_kategori, keterangan) VALUES ('$nama_kategori','$keterangan')") or die (mysqli_error($con));
     if($insert){
-        $success = 'Berhasil menambahkan data kategori';
+        $success = 'berhasil menambahkan data kategori';
     }else{
-        $error = 'Gagal menambahkan data kategori';
+        $error = 'gagal menambahkan data kategori';
     }
     $_SESSION['success'] = $success;
     $_SESSION['error'] = $error;
@@ -23,9 +23,9 @@ if(decrypt($_GET['act'])=='delete' && isset($_GET['id'])!=""){
     $id = decrypt($_GET['id']);
     $query = mysqli_query($con, "DELETE FROM kategori WHERE idkategori='$id'")or die(mysqli_error($con));
     if($query){
-        $success = 'Berhasil menghapus data kategori';
+        $success = 'berhasil menghapus data kategori';
     }else{
-        $error = 'Gagal menghapus data kategori';
+        $error = 'gagal menghapus data kategori';
     }
     $_SESSION['success'] = $success;
     $_SESSION['error'] = $error;
